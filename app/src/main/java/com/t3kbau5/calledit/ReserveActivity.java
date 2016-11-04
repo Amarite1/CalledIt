7a726d6b5d6ec7f1e03242d8ae9fda01fba9766d
Ben W <me@t3kbau5.com>
2016-11-04 02:01:17 -0400

    Allow User Account Saving To facilitate the login process, the user can now enter their username to be autofilled whenever the login page appears for booking a room. For security purposes, their password will not be requested or stored.

--- app/src/main/java/com/t3kbau5/calledit/ReserveActivity.java
+++ app/src/main/java/com/t3kbau5/calledit/ReserveActivity.java
@@ -1,22 +1,28 @@
 package com.t3kbau5.calledit;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.webkit.CookieManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
 
 public class ReserveActivity extends AppCompatActivity {
 
     private Activity _this = this;
     private WebView webView;
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +37,14 @@ public class ReserveActivity extends AppCompatActivity {
             url += "?space=" + intent.getIntExtra("roomID", -1);
         }
 
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
         webView = (WebView) findViewById(R.id.webView);
         WebSettings ws = webView.getSettings();
         ws.setJavaScriptEnabled(true);
 
         final CookieManager cm = CookieManager.getInstance();
         cm.setAcceptCookie(true);
-        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         String savedCookies = prefs.getString("cookies", "");
         if(!savedCookies.equals("")){
             cm.setCookie(prefs.getString("curl", ""), savedCookies);
@@ -49,7 +56,9 @@ public class ReserveActivity extends AppCompatActivity {
                 Log.d("wv", "opf");
                 if(webView.getUrl().toLowerCase().contains("login")){
                     Log.d("wv", "login");
-                    //webView.loadUrl("javascript:(function(){$('#txtUsername').val('" + username + "');$('#pwdPassword').val('" + password + "');})()");
+                    if(prefs.contains("username")) {
+                        webView.loadUrl("javascript:(function(){$('#txtUsername').val('" + prefs.getString("username", "") + "');})()");
+                    }
                     prefs.edit().putString("cookies", cm.getCookie(url)).putString("curl", url).apply();
                 }
             }
@@ -58,11 +67,44 @@ public class ReserveActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = new MenuInflater(this);
+        inflater.inflate(R.menu.activity_reserve, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
                 //NavUtils.navigateUpFromSameTask(this);
                 finish();
+                return true;
+            case R.id.menu_username:
+
+                AlertDialog.Builder adb = new AlertDialog.Builder(this);
+                adb.setTitle("Set Saved Username");
+
+                final EditText et = new EditText(this);
+                if(prefs.contains("username")) et.setText(prefs.getString("username", ""));
+                et.setHint("12ab34");
+
+                adb.setView(et);
+                adb.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        PreferenceManager.getDefaultSharedPreferences(_this).edit().putString("username", et.getText().toString()).apply();
+                    }
+                });
+                adb.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                adb.show();
+
+
                 return true;
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/t3kbau5/calledit/RoomListAdapter.java
+++ app/src/main/java/com/t3kbau5/calledit/RoomListAdapter.java
@@ -10,6 +10,8 @@ import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -59,15 +61,45 @@ public class RoomListAdapter extends BaseAdapter {
         }else{
             ll = (LinearLayout) inflater.inflate(R.layout.listitem_room, null);
         }
-        ((TextView) ll.findViewById(R.id.text_name)).setText(roomData.get(position).name);
+
+        final Room room = roomData.get(position);
+
+        ((TextView) ll.findViewById(R.id.text_name)).setText(room.name);
         ll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, RoomInfoActivity.class);
-                intent.putExtra("room", roomData.get(position));
+                intent.putExtra("room", room);
                 context.startActivity(intent);
             }
         });
+
+        if(room.hasPhone){
+            ll.findViewById(R.id.hasPhone).setVisibility(View.VISIBLE);
+        }else{
+            ll.findViewById(R.id.hasPhone).setVisibility(View.INVISIBLE);
+        }
+
+        if(room.hasTV){
+            ll.findViewById(R.id.hasPhone).setVisibility(View.VISIBLE);
+        }else{
+            ll.findViewById(R.id.hasPhone).setVisibility(View.INVISIBLE);
+        }
+
+        TextView sizeText = (TextView) ll.findViewById(R.id.roomSize);
+
+        switch(room.size){
+            case 1:
+                sizeText.setText("S");
+                break;
+            case 2:
+                sizeText.setText("M");
+                break;
+            case 3:
+                sizeText.setText("L");
+                break;
+        }
+
         return ll;
     }
 }
